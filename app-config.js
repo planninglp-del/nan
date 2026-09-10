e406739364e3a2b4ee762f477d7b4e0a090b54c1
planninglp-del <planning_lp@lpn.nfe.go.th>
2026-09-10 14:57:20 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: ''
+    API_URL: 'https://script.google.com/macros/s/AKfycbxco8sAmn3mx2GDJWjyWh2HYcFvsKOCx3VwyuLzFdol2LnJ_4hE96TuWHUwd8RsJB5u/exec'
   });
 })();
