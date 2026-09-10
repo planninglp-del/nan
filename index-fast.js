ca234f9f5e0c5af6b6aea316c965c08b8b383223
planninglp-del <planning_lp@lpn.nfe.go.th>
2026-09-10 14:59:39 +0700

    Update index-fast.js

--- index-fast.js
+++ index-fast.js
@@ -1513,7 +1513,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbytykna5KaU7ucv6ZIXl0pkIfigEz0mRY4mmVuKUyATNr2rT7qcgSVaZl58v15icqiY/exec';
 
 ;
 
